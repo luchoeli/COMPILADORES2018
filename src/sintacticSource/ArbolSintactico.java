53c2f7fa9563050fa10167946f4725b8cf0f9f25
Adrian Guerrero <adrianpabloguerrero@gmail.com>
2018-10-10 15:05:19 -0300

    print

--- src/sintacticSource/ArbolSintactico.java
+++ src/sintacticSource/ArbolSintactico.java
@@ -13,4 +13,25 @@ public class ArbolSintactico {
 		nodos.add(n);
 	}
 
+	
+	public void imprimirArbol () {
+		
+		for (int i=0; i<nodos.size(); i++) {
+			System.out.println("---------- NUEVO SUBARBOL -----------");
+				imprimirSubArbol (nodos.get(i));
+		}
+		
+	}
+	
+	private void imprimirSubArbol (Nodo n) {
+		if ((n.getDer() == null) && (n.getIzq() == null)){
+			n.imprimirNodo();
+		}
+		else {
+			imprimirSubArbol (n.getIzq());
+			imprimirSubArbol (n.getDer());
+		}
+			
+		
+	}
 }
